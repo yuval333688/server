1683b5cdee8ac935be8fdc95ed09814caba9e628
yuval333688 <yuvalgefen11@gmail.com>
2025-06-10 15:39:50 +0300

    Add getNames route to return a list of names

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+
+@app.route("/getNames")
+def getNames():
+    print("getNames called\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    return "James Mary John Patricia Robert Jennifer Michael Linda William Elizabeth"
