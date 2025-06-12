df9e278719cec2e60b2a946df8cf50fbf8cea8d9
yuval333688 <yuvalgefen11@gmail.com>
2025-06-12 13:14:45 +0300

    Add DataBaseManager and implement getCountryBorder route; update requirements and .gitignore

---
+++ DataBaseManager.py
@@ -0,0 +1,51 @@
+import os
+from supabase import create_client, Client
+
+
+
+class DataBaseManager:
+    instance: DataBaseManager = None
+
+    def __init__(self):
+        if DataBaseManager.instance is None:
+            DataBaseManager.instance = self
+            self.supabase = self.initialize_supabase()
+
+
+
+    def initialize_supabase(self):
+        url: str = os.environ.get("SUPABASE_URL")
+        key: str = os.environ.get("SUPABASE_KEY")
+        supabase: Client = create_client(url, key)
+        return supabase
+
+    def getCountryBorder(self,CountryName:str):
+        response = ( self.supabase.table("countryBorder")
+                .select("border")
+                .eq("name", "Canada")
+                .execute())
+        
+        arr  =  response.data[0]['border']
+        borderarr = []
+        for i in range(len(arr)):
+            coords = list(map(int, arr[i].split(',')))
+            borderarr.append(coords)
+        return borderarr
+
+            
+            
+        
+           
+        
+        
+    
+
+
+
+  
+
+        
+        
+        
+        
+       
--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
+from DataBaseManager import DataBaseManager
 app = Flask(__name__)
+
+
+
 
 @app.route("/")
 def hello_world():
@@ -9,6 +13,17 @@ def hello_world():
 
 @app.route("/getNames")
 def getNames():
+    
     print("getNames called\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     names = ["James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth"]
     return jsonify(names)
+
+@app.route("/getCountryBorder/<country>")
+def getCountryBorder(country):
+    dataBaseManager = DataBaseManager()
+    borders = dataBaseManager.fetch_CountryBorder(country)
+    return jsonify(borders)
+
+if __name__ == '__main__':
+    # Initialize Supabase client
+    dataBaseManager = DataBaseManager()
